0fbb4af6dda288d7f009160425ed41bc6bf32344
Rakhimbekov Rakhat <ahat0405@mail.ru>
2023-12-20 20:11:18 +0600

    1st assignment

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,44 @@
-public class Main {
-    private static void print(){
-        System.out.print("Hello World!");
+class Person {
+    public String name;
+    public int age;
+    public Person() {
+
     }
+    public Person(String name, int age) {
+        this();
+        setName(name);
+        setAge(age);
+    }
+    public Person(String name) {
+        this();
+        setName(name);
+    }
+    public Person(int age) {
+        this();
+        setAge(age);
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void printDetails() {
+        System.out.println(getName() + " " + getAge());
+    }
+}
+public class Main {
     public static void main(String[] args) {
-        print();
+        Person i = new Person();
+        Person he = new Person("Arsen", 18);
+        i.setName("Rakhimbekov Rakhat");
+        i.setAge(18);
+        i.printDetails();
     }
 }
